38c9558a9a2a67eb226ad5adcae3fbfe8314ff28
fascinate98 <fascinate982@naver.com>
2022-03-14 12:42:49 +0900

    react #3

---
+++ 2.Component/ex02/src/01/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+ const App = () => {
+
+  const h1Style = {
+    width : 280,
+        height: 50,
+        backgroundColor : "#eeff00"
+  };
+
+  return (
+    <div id = "App">
+      <h1 style ={
+        h1Style
+      }>Inline Styling</h1>
+    </div>
+  )
+}
+export default App
